04caadc9540c6dfc9471b3e2c43b0e200f40ba52
masternati <sohrabhossain207@gmail.com>
2023-02-11 10:21:58 +0600

    sample codes added

---
+++ JS.js
@@ -0,0 +1,24 @@
+// console.log("tosher");
+// let x,y,z
+// x = 5 + 6;
+// y = x * 10;
+// z=x+y
+// console.log(z);
+
+// let person = "Hege"
+// console.log(person);
+
+// var a="John"
+// let b="Doe"
+// let c= a + " "  + b
+// console.log(c);
+
+// const price1 = 5;
+// const price2 = 6;
+// let total = price1 + price2;
+// console.log(total);
+
+// let s = "Volvo" + 16 + 4;
+// console.log(s);
+
+
